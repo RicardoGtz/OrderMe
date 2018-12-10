9c11a69f8ec7d81a6248c6b49151ab8c58bf4f77
TioTartas <alanzamora.9780@gmail.com>
2018-12-10 03:12:30 -0600

    Merge remote-tracking branch 'origin/master' into Alan-Branch

--- ciudad.php
+++ ciudad.php
@@ -4,7 +4,7 @@
   include('connectmysql.php');
 if(isset($_GET['delete_id']))//Si esta puesto el get entonces se ejecuta, dice delete id pero realmente puede llevar cualquier valor, solo es renombrar la variable abajo en el boton
 {
-  $ciu=$_GET['delete_id'];//le doy el valor de los GET a variables ya que si lo hacia directo habia problemas con las comillas (cosas raras), 
+  $ciu=$_GET['delete_id'];//le doy el valor de los GET a variables ya que si lo hacia directo habia problemas con las comillas (cosas raras),
   $pro=$_GET['provinc'];
   $sql_query="call EliminarCiudad('$ciu','$pro')";
   $r= @mysqli_query($dbcon,$sql_query);
@@ -32,11 +32,11 @@ if(isset($_GET['delete_id']))//Si esta puesto el get entonces se ejecuta, dice d
   <h1 class="courgete">Ciudades</h1>
   <p></p>
   <p class="centrado">A continuacion, se mostrara el catalogo de los las ciudades donde trabaja la empresa Farmacias "El Dorado"</p>
-    <?php 
+    <?php
       if (@$_SESSION['user'] == 'administradorG'){
         echo "<div class='centrado'><input class='boto' type='button' name='insert' value='Insertar' onclick=location.href='cityinsert.php'></div>";
       }
-    ?>  
+    ?>
   <p></p>
   <div class="table-responsive">
     <table class="table table-striped">
@@ -44,7 +44,7 @@ if(isset($_GET['delete_id']))//Si esta puesto el get entonces se ejecuta, dice d
           <tr>
             <th>Nombre</th>
             <th>Provincia</th>
-            <?php 
+            <?php
             if (@$_SESSION['user'] == 'administradorG'){
               echo "<th>Editar</th>";
               echo "<th>Eliminar</th>";
@@ -55,7 +55,7 @@ if(isset($_GET['delete_id']))//Si esta puesto el get entonces se ejecuta, dice d
         <tbody>
           <?php
             include('connectmysql.php');
-            
+
             $sqldata= mysqli_query($dbcon,"call VerCiudad()");
 
             while($row=mysqli_fetch_array($sqldata,MYSQLI_NUM)){
@@ -65,7 +65,7 @@ if(isset($_GET['delete_id']))//Si esta puesto el get entonces se ejecuta, dice d
               echo utf8_encode($row[1]);
               echo "</td>";
               if (@$_SESSION['user'] == 'administradorG'){
-                echo "<td><a href='cityinsert.php?id=$row[0]&p=$row[1]'><img src='comun/img/sistema/act2.png' class='img-rounded'></td>";
+                echo "<td><a href='ciuinsert.php?id=$row[0]&p=$row[1]'><img src='comun/img/sistema/act2.png' class='img-rounded'></td>";
                 echo "<td><a href='ciudad.php?delete_id=$row[0]&provinc=$row[1]' onclick='return confirm('sure to delete !');'><img src='comun/img/sistema/eli2.png' alt='Delete' class='img-rounded'/></a></td>";
                 echo "<tr>";
               }
