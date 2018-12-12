0fd5498c42d245fc8a834382801e2c6f87cf9fde
TioTartas <alanzamora.9780@gmail.com>
2018-12-11 22:31:14 -0600

    Ya no se que estoy haciendo

--- sucursalInsert.php
+++ sucursalInsert.php
@@ -11,11 +11,77 @@
 <?php
 	//include('includes/global.php');
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		include ('connectmysql.php');
 		$errors = array();
-		$errors=checarUsuario($errors);
+		if (empty($_POST['id_sucursal']))
+			$errors[] = 'Olvido introducir el ID de la Sucursal';
+		else{
+			$sucursal = trim($_POST['id_sucursal']);
+			$sucursal = mysqli_real_escape_string($dbcon, $sucursal);
+		}
+
+		if (empty($_POST['nombre']))
+			$errors[] = 'Olvido introducir el Nombre de la Sucursal';
+		else{
+			$nombre = trim($_POST['nombre']);
+			$nombre = mysqli_real_escape_string($dbcon, $nombre);
+		}
+
+		if (empty($_POST['estado']))
+			$errors[] = 'Olvido introducir el Estado';
+		else{
+			$estado = trim($_POST['estado']);
+			$estado = mysqli_real_escape_string($dbcon, $estado);
+		}
+
+		if (empty($_POST['ciudad']))
+			$errors[] = 'Olvido introducir la Ciudad';
+		else{
+			$ciudad = trim($_POST['ciudad']);
+			$ciudad = mysqli_real_escape_string($dbcon, $ciudad);
+		}
+
+		if (empty($_POST['direccion']))
+			$errors[] = 'Olvido introducir la direccion';
+		else{
+			$direccion = trim($_POST['direccion']);
+			$direccion = mysqli_real_escape_string($dbcon, $direccion);
+		}
+
+		if (empty($_POST['h_apertura']))
+			$errors[] = 'Olvido introducir el horario de apertura';
+		else{
+			$apertura = trim($_POST['h_apertura']);
+			$apertura = mysqli_real_escape_string($dbcon, $apertura);
+		}
+
+		if (empty($_POST['h_cierre']))
+			$errors[] = 'Olvido introducir el horario de cierre';
+		else{
+			$cierre = trim($_POST['h_cierre']);
+			$cierre = mysqli_real_escape_string($dbcon, $cierre);
+		}
+
+		if (empty($_POST['telefono']))
+			$errors[] = 'Olvido introducir el nombre del usuario';
+		else{
+			$telefono = trim($_POST['telefono']);
+			$telefono = mysqli_real_escape_string($dbcon, $telefono);
+		}
+
+		if (empty($_POST['id_restaurante']))
+			$errors[] = 'Olvido introducir el nombre del usuario';
+		else{
+			$restaurante = trim($_POST['id_restaurante']);
+			$restaurante = mysqli_real_escape_string($dbcon, $restaurante);
+		}
 
 		if (empty($errors)){
-			$resp=insertarUsuario();
+			$query="select InsertarSucursal('$sucursal','$nombre','$ciudad','$estado','$direccion','$apertura','$cierre','$telefono','$restaurante') as resp";
+			$res=@mysqli_query($dbcon,$query);
+			$row=mysqli_fetch_assoc($res);
+			$resp=$row['resp'];
+			mysqli_close($dbcon);
 
 			if($resp==1){
 				echo '<h1>Muchas gracias!</h1>
@@ -30,7 +96,6 @@
 				echo '<h1>Atencion</h1>
 							<p>Ya existe esa sucursal!</p><p><br /></p>';
 			}
-
 		}
 	}// Fin de acciones cuando se envía el formulario
 ?>
@@ -44,7 +109,7 @@
 				<p>ID de la sucursal</p><input type="text" name="id_sucursal" required maxlength="7" value=""><br>
 				<p>Nombre de la Sucursal</p><input type="text" name="nombre" required maxlength="40" value=""><br>
 				<!-- AQUI DESPLEGAR LISTA DE CIUDADES Y ESTADOS -->
-				<p>Estado:</p><input type="text" mane="estado" required maxlength="40" value=""><br>
+				<p>Estado:</p><input type="text" name="estado" required maxlength="40" value=""><br>
 				<p>Ciudad:</p><input type="text" name="ciudad" required maxlength="40" value=""><br>
 				<!-- ******************************** -->
 				<p>Direccion:</p><input type="text" name="direccion" required maxlength="40" value=""><br>
