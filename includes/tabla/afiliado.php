a10ed3050932d9d6088cc62e128f5732a5b93198
AVCA <anavca@outlook.com>
2018-12-12 18:24:39 -0600

    Paginas listas: Index.php Afiliados.php Sucursales.php Platillos.php

--- includes/tabla/afiliado.php
+++ includes/tabla/afiliado.php
@@ -3,15 +3,20 @@
 		$sqldata= mysqli_query($dbcon,"call VerRestaurante()");
             while($row=mysqli_fetch_array($sqldata,MYSQLI_NUM)){
               	echo '<table class="table table-striped">';
-              	echo '<tr><th colspan="4" scope="col">'.utf8_encode($row[1]).'</th></tr scope="row">';
-              	echo '<tr><td colspan="4">';
-              	echo "<br/>";
-              	echo "
-              		<a href='sucursales.php?id=$row[0]&p=$row[1]' class='btn btn-secondary' role='button' aria-pressed='true'>Ver Sucursales</a>
-              	";
-              	echo "<br/>";
-				echo '</td><tr>';
-				echo "<br/>";
-				echo '</table>';
+                	echo '<tr>';
+                    echo '';
+                    echo '<td>'.utf8_encode($row[1]).'';
+                    echo'</td>';
+                    echo "<td>";
+                    echo "<div class='text-right'>";
+                    echo "
+                    <a href='sucursales.php?id=$row[0]&p=$row[1]' class='btn btn-secondary ' role='button' aria-pressed='true'>Ver Sucursales</a>
+                    ";
+                    echo "</div>";
+                    echo'</td>';
+                  echo '</tr>';
+  				        echo "<br/>";
+				        echo '</table>';
             }
 ?>
+
