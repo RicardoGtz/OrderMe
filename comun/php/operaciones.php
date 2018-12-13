f04e954a7eb4daf867d030fe50994979823aef10
AVCA <anavca@outlook.com>
2018-12-12 21:59:25 -0600

    Empleado: Ver perfil listo

--- comun/php/operaciones.php
+++ comun/php/operaciones.php
@@ -39,6 +39,33 @@ switch ($tabla) {
                 break;
         }
         break;
+        case 'Empleado':
+        if($operacion!='eliminar' && $operacion!='iniciar')
+        {
+            $var1 = $_POST['var1'];
+            $var2 = $_POST['var2'];
+            $var3 = $_POST['var3'];
+            $var4 = $_POST['var4'];
+            $var5 = $_POST['var5'];
+            $var6 = $_POST['var6'];
+        }
+        else{
+            $var1 = $_POST['var1'];
+            $var2 = $_POST['var2'];
+        }
+
+        switch ($operacion) {
+            case 'agregar':
+                //$sql  = "select InsertarUsuario('$var1','$var2','$var3','$var4','$var5','$var6','$var7','$var8','$var9','$var10') as resp";
+                break;
+            case 'actualizar':
+                $sql = "select ActualizarEmpleado('$var1','$var1','$var2','$var3','$var4','$var5','$var6') as resp";
+                break;
+            case 'eliminar':
+                //$sql = "call e_eliminar_vt('$var1','$var2','$valor');";
+                break;
+        }
+        break;
 }
 
 $res=@mysqli_query($dbcon,$sql);
--- perfilEmpleado.php
+++ perfilEmpleado.php
@@ -1,121 +1,299 @@
+<?php
+session_start();
+include('connectmysql.php');
+$usuario = @$_SESSION['user'];
+$id=@$_SESSION['usuario'];
+$sqldata=mysqli_query($dbcon,"call VerEmpleadoEspecifico('$id')");
+$row=mysqli_fetch_array($sqldata,MYSQLI_NUM);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-<head>
-	<title>Registro - OrderMe</title>
-	<?php include('includes/links.php'); ?>
-</head>
-<?php
-	include('includes/global.php');
-	crearHeaders();
-    include('connectmysql.php');
+	<head>
+	        <meta charset="utf-8">
+	        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	        <title>
+	          Order Me
+	        </title>
+	        <link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" rel="stylesheet">
+	        <link href="comun/librerias/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	        <script crossorigin="anonymous" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js">
+	        </script>
+	        <script crossorigin="anonymous" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" src="https://code.jquery.com/jquery-3.2.1.slim.min.js">
+	        </script>
+	        <script crossorigin="anonymous" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+	        </script>
 
-    $id=@$_SESSION['usuario'];
-<<<<<<< HEAD
-    echo "El ID ".$id;
-=======
-    
->>>>>>> origin/master
+	        <!-- Online -->
+	        <link href="https://fonts.googleapis.com/css?family=Josefin+Slab|Lobster|Raleway|Playball" rel="stylesheet"/>
+	        <link href="comun/librerias/icon/css/font-awesome.min.css" rel="stylesheet">
 
-?>
-<?php
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	        <link href="comun/librerias/alertifyjs/css/alertify.css" rel="stylesheet" type="text/css"/>
+	        <link href="comun/librerias/alertifyjs/css/themes/default.css" rel="stylesheet" type="text/css"/>
+	        <link href="comun/librerias/select2/css/select2.css" rel="stylesheet" type="text/css"/>
+	        <link href="comun/css/main.css" rel="stylesheet"/>
+	        <link href="comun/css/animate.css" rel="stylesheet" type="text/css"/>
+	        <script src="comun/librerias/jquery-3.2.1.min.js">
+	        </script>
+	        <script src="comun/js/operaciones.js">
+	        </script>
+	        <script src="comun/librerias/bootstrap/js/bootstrap.js">
+	        </script>
+	        <script src="comun/librerias/alertifyjs/alertify.js">
+	        </script>
+	        <script src="comun/librerias/select2/js/select2.js">
+	        </script>
+	        </link>
+	        </link>
+	        </meta>
+	        </meta>
+	</head>
+	<body>
+	        <!-- Encabezado -->
+	        <div class="col-lg-10 col-md-10 col-sm-10 mx-auto text-left espacio-arriba">
+	          <div class="row">
+	            <div class="col-md-4">
+	              <h1 class="Font_Playball mediano_4 Verde_logo animated fadeIn">
+	                Order Me
+	              </h1>
+	            </div>
+	            <!-- Boton -->
+	            <div id = "botones" class="col order-12 offset-md-1 offset-lg-5">
+	            </div>
+	          </div>
 
-		//include ('connectmysql.php');
-		$errors = array();
-		if (empty($_POST['id_empleado']))
-			$errors[] = 'Olvido introducir el ID del Usuario';
-		else{
-			$ni = trim($_POST['id_empleado']);
-			$ni = mysqli_real_escape_string($dbcon, $ni);
-		}
-
-		if (empty($_POST['nombre']))
-			$errors[] = 'Olvido introducir el Nombre del Usuario';
-		else{
-			$nn = trim($_POST['nombre']);
-			$nn = mysqli_real_escape_string($dbcon, $nn);
-		}
-
-		if (empty($_POST['correo']))
-			$errors[] = 'Olvido introducir el Correo del Usuario';
-		else{
-			$nc = trim($_POST['correo']);
-			$nc = mysqli_real_escape_string($dbcon, $nc);
-		}
-
-		if (empty($_POST['pass']))
-			$errors[] = 'Olvido introducir la Contraseña';
-		else{
-			$np = trim($_POST['pass']);
-			$np = mysqli_real_escape_string($dbcon, $np);
-		}
-
-		if (empty($_POST['telefono']))
-			$errors[] = 'Olvido introducir el nombre del usuario';
-		else{
-			$nt = trim($_POST['telefono']);
-			$nt = mysqli_real_escape_string($dbcon, $nt);
-		}
-
-		if (empty($_POST['id_sucursal']))
-			$errors[] = 'Olvido introducir el nombre del usuario';
-		else{
-			$ns = trim($_POST['id_sucursal']);
-			$ns = mysqli_real_escape_string($dbcon, $ns);
-		}
-
-		if (empty($errors)){
-			$query="select ActualizarEmpleado('$ni','$ni','$nn','$np','$nc','$nt','$ns') as resp";
-			$res=@mysqli_query($dbcon,$query);
-			$row=mysqli_fetch_assoc($res);
-			$resp=$row['resp'];
-			mysqli_close($dbcon);
-
-			if($resp==1){
-				echo '<h1>Muchas gracias!</h1>
-				<p>Sus datos han sido actualizados en la base de datos!</p><p><br /></p>';
-			}else if($resp==2){
-				echo '<h1>Atencion</h1>
-				<p>No existe esa sucursal!</p><p><br /></p>';
-			}else if($resp==3) {
-        echo '<h1>Atencion</h1>
-				<p>El ID de empleado ya existe!</p><p><br /></p>';
-      }else if($resp==0)
-        echo '<h1>Atencion</h1>
-        <p>No existe ese empleado</p><p><br /></p>';
-		}else{
-      echo $errors[0];
-    }
-    include('includes/footer.html');
-    exit();
-	}// Fin de acciones cuando se envía el formulario
-
-
-?>
-<body>
-	<div class="contenedor">
-		<h1>Registrate</h1>
-		<p></p>
-		<p class="centrado">Por favor asegurate de llenar todos los campos del formulario para poder agregar la informacion al sistema</p>
-		<div class="contenedor col-md-3 center-block fondoazul">
-        <?php
-            //$id=$_GET['id'];
-            $sqldata=mysqli_query($dbcon,"call VerEmpleadoEspecifico('$id')");
-            $row=mysqli_fetch_array($sqldata,MYSQLI_NUM);
-
-            echo '<form action="perfilEmpleado.php" method="POST">';
-            echo '<p>ID del Empleado:</p><input type="text" name="id_empleado" required maxlength="7" value="'.utf8_encode($row[0]).'" readonly><br>';
-            echo '<p>Nombre:</p><input type="text" name="nombre" required maxlength="40" value="'.utf8_encode($row[1]).'"><br>';
-            echo '<p>Contraseña:</p><input type="password" name="pass" required maxlength="10" value="'.utf8_encode($row[2]).'"><br>';
-            echo '<p>Correo:</p><input type="text" name="correo" required maxlength="40" value="'.utf8_encode($row[3]).'"><br>';
-            echo '<p>Telefono:</p><input type="text" name="telefono" required maxlength="10" value="'.utf8_encode($row[4]).'"><br>';
-            echo '<p>ID de sucursal:</p><input type="text" name="id_sucursal" required maxlength="7" value="'.utf8_encode($row[5]).' "readonly><br>';
-            echo '<input type="submit" value="Actualizar" class="btn btn-success btn-primary">';
-        ?>
-			</form>
-		</div>
-	<p></p>
-	</div>
-	<?php include('includes/footer.html'); ?>
-</body>
+	          <h2 class="Font_Raleway mediano animated fadeIn retraso-1 Gris espacio-abajo">
+	            Ordena comida sin meseros (owo)/
+	          </h2>
+	          <label for="">
+	          </label>
+	        </div>
+	        <!-- Menu -->
+	        <div class="" id="menu">
+	        </div>
+	        <!-- Contenido -->
+		    <div class="animated fadeIn retraso-2 mx-auto">
+		    <!-- Texto -->
+		    <h2 class="Font_Raleway Dorado mediano_2 text-center mx-auto col-md-10 espacio-arriba">
+		    Datos de la cuenta
+		    </h2>
+		    <!-- Area 1 -->
+		    <div class=" col-md-10 mx-auto Negro espacio-abajo">
+		        <!-- Linea divisora -->
+		        <hr style="color: #0056b2;"/>
+		        <!-- Articulos -->
+		        <p class="mediano text-center">Por favor asegurate de llenar todos los campos del formulario para poder agregar la informacion al sistema.</p>
+		        <div class="col-lg-8 col-md-8 col-sm-8 mx-auto quitar-float espacio-arriba espacio-abajo text-left" id ="formulario">
+		        	<div class="modal-dialog modal-lg" role="document">
+        				<div class="modal-content">
+				            <div class="modal-body">
+				                <label>
+				                    Usuario:
+				                </label>
+				                <input tabindex="2" aria-describedby="basic-addon2" aria-label="Usuario" class="form-control input-sm rounded" id="r_var1" maxlength="7" onkeypress="" placeholder="Usuario" required="" type="text" value="<?php echo $row[0] ?>" />
+				                <label>
+				                    Nombre:
+				                </label>
+				                <input tabindex="3" aria-describedby="basic-addon2" aria-label="Nombre" class="form-control input-sm rounded" id="r_var2" maxlength="40" onkeypress="return soloLetras(event);" placeholder="Nombre" required="" type="text" value="<?php echo $row[1] ?>"/>
+				                <label>
+				                    Contraseña:
+				                </label>
+				                <input tabindex="5" aria-describedby="basic-addon2" aria-label="Contraseña" class="form-control input-sm rounded" id="r_var3" maxlength="10" onkeypress="" placeholder="Contraseña" required="" type="password" value="<?php echo $row[2] ?>"/>
+				                <label>
+				                    Correo:
+				                </label>
+				                <input tabindex="4" aria-describedby="basic-addon2" aria-label="Correo" class="form-control input-sm rounded" id="r_var4" maxlength="40" placeholder="Correo@" required="" type="text" value="<?php echo $row[3] ?>"/>
+				                <label>
+				                    Telefono:
+				                </label>
+				                <input tabindex="6" aria-describedby="basic-addon2" aria-label="Telefono" class="form-control input-sm rounded" id="r_var5" maxlength="10" onkeypress="return ;" placeholder="Telefono" required="" type="text" value="<?php echo $row[4] ?>"/>
+				                <label>
+				                    Id de la sucursal:
+				                </label>
+				                <input  tabindex="7" aria-describedby="basic-addon2" aria-label="Numero de tarjeta" class="form-control input-sm rounded" id="r_var6" maxlength="16" onkeypress="" placeholder="" required="" type="text" value="<?php echo $row[5] ?>"/>
+				            </div>
+				            <div class="modal-footer">
+				                <button tabindex="12" class="btn btn-info" data-dismiss="modal" id="actualizar" type="button">
+				                    Registrarse
+				                </button>
+				            </div>
+		        		</div>
+		        	</div>
+		        </div>
+		    </div> 
+		    <!--- Footer -->
+		    <footer class="footer-bs">
+		        <div class="row">
+		            <div class="col-md-3 footer-brand animated fadeInLeft">
+		                <h2>
+		                    Order Me
+		                </h2>
+		                <p>
+		                    Equipo:
+		                    Ana Victoria Cavazos Argot
+		                    Andres Graciano López
+		                    Ricardo Gutierrez Otero
+		                    Jose Miguel Rodriguez Reyes
+		                    Alan Francisco Zamora Barrera
+		                </p>
+		                <p>
+		                    © 2018 All rights reserved
+		                </p>
+		            </div>
+		            <div class="col-md-4 footer-nav animated fadeInUp">
+		                <h4>
+		                    Secciones
+		                </h4>
+		                <div class="col-md-6">
+		                </div>
+		                <div class="col-md-6">
+		                    <ul class="list">
+		                        <li>
+		                            <a href="#">
+		                                Inicio
+		                            </a>
+		                        </li>
+		                        <li>
+		                            <a href="#">
+		                                Afiliados
+		                            </a>
+		                        </li>
+		                    </ul>
+		                </div>
+		            </div>
+		            <div class="col-md-2 footer-social animated fadeInDown">
+		                <h4>
+		                    Siguenos:
+		                </h4>
+		                <ul>
+		                    <li>
+		                        <a href="#">
+		                            Facebook
+		                        </a>
+		                    </li>
+		                    <li>
+		                        <a href="#">
+		                            Twitter
+		                        </a>
+		                    </li>
+		                    <li>
+		                        <a href="#">
+		                            Instagram
+		                        </a>
+		                    </li>      
+		                </ul>
+		            </div>
+		            <div class="col-md-3 footer-ns animated fadeInRight">
+		                <h4>
+		                    Novedades:
+		                </h4>
+		                <p>
+		                    Los germinados son uno de los pocos alimentos que ingerimos cuando aún están vivos, lo cual aumenta enormemente su valor nutricional. Las semillas...
+		                </p>
+		            </div>
+		        </div>
+		    </footer>
+		    </div>
+	</body>
 </html>
+
+
+<!-- Cargar elementos -->
+<script type="text/javascript">
+    $(document).ready(function(){
+    $('#menu').load("includes/menu/menu.php");
+    $('#botones').load("includes/botones/botones.php");
+    $('#body').load("includes/body/body.php");
+    $('#contenido').load("includes/contenido/contenido_afiliado.php");
+  });
+</script>
+<!-- VALIDACION DE ESCRITURA -->
+<script>
+    function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toString();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890";//Se define todo el abecedario que se quiere que se muestre.
+    especiales = [8, 46, 6,9]; //Es la validación del KeyCodes, que teclas recibe el campo de texto.
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial){
+        return false;
+    }
+    }
+</script>
+<script>
+    function soloNumeros(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toString();
+    letras = ".1234567890";//Se define todo el abecedario que se quiere que se muestre.
+    especiales = [8, 46, 6,9]; //Es la validación del KeyCodes, que teclas recibe el campo de texto.
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial){
+        return false;
+    }
+    }
+</script>
+<!-- REALIZAR OPERACIONES -->
+<script type="text/javascript">
+    $(document).ready(function() {
+        // Registro:
+        $('#actualizar').click(function() {
+            // Obtenemos los datos del formulario
+            var1 = $('#r_var1').val();
+            var2 = $('#r_var2').val();
+            var3 = $('#r_var3').val();
+            var4 = $('#r_var4').val();
+            var5 = $('#r_var5').val();
+            var6 = $('#r_var6').val();
+
+            // Comenzamos con el update
+            tabla = 'Empleado';
+            operacion = 'actualizar';
+            cadena="tabla="+tabla+
+            "&operacion="+operacion+
+            "&var1="+var1+
+            "&var2="+var2+
+            "&var3="+var3+
+            "&var4="+var4+
+            "&var5="+var5+
+            "&var6="+var6; 
+             
+            if(var1 != "" &&
+                var2 != "" &&
+                var3 != "" &&
+                var4 != "" &&
+                var5 != "" &&
+                var6 != "" ){
+            	alertify.confirm('Actualizar cambios', '¿Desea guardar los cambios?', function(){ actualizar_empleado(cadena,tabla); }
+                , function(){ window.location="perfilCliente.php";});   
+            }
+            else
+            {        
+                cadena="Campos vacios = ";
+                if(var1=="") cadena = cadena+"Usario |"; 
+                if(var2=="") cadena = cadena+"Nombre |"; 
+                if(var3=="") cadena = cadena+"Correo |"; 
+                if(var4=="") cadena = cadena+"Contraseña |"; 
+                if(var5=="") cadena = cadena+"Telefono |"; 
+                if(var6=="") cadena = cadena+"Numero de tarjeta |";
+                alertify.alert("Datos incompletos: ",cadena);
+            }          
+        });
+  });
+</script>
+
